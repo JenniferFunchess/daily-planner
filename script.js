a077c8eccf5062c711b4a72c0a5fb8dbf29a718f
Jennifer Funchess <jfunchess@ymail.com>
2020-10-06 22:12:26 -0400

    reorganized the rows and columns in the html

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
   console.log("This is loading!");
+
+  //DOM VARIABLES
+
+  $("#saveIcon1").on("click", function () {
+    console.log(this);
+    var text = $("#Row9").val().trim();
+    var time = $("#nine").attr("id");
+    localStorage.setItem(time, text);
+    console.log(localStorage);
+  });
+  //EVENT LISTENERS
 });
